00672777d56abd646e35b7890806fd8ee89dcae5
Kuoz2 <david.palta.anes1989@gmail.com>
2022-07-13 00:59:50 -0400

    subiendo actualizacion

--- db/migrate/20220329172358_add_valor_pvalor.rb
+++
@@ -1,6 +0,0 @@
-class AddValorPvalor < ActiveRecord::Migration[6.0]
-  def change
-    add_column :codes, :pvalor, :bigint, :default => 0
-    #Ex:- :default =>''
-  end
-end
--- db/migrate/20220329172929_add_vendidos_voucher.rb
+++
@@ -1,5 +0,0 @@
-class AddVendidosVoucher < ActiveRecord::Migration[6.0]
-  def change
-    add_column :codes, :voucher_vendido, :boolean, :default => false
-  end
-end
