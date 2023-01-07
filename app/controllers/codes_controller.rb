dea923614d903f570c0cc4795652ba84142a5278
Kuoz2 <david.palta.anes1989@gmail.com>
2023-01-06 21:53:56 -0300

    suebiendo cosas

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -98,7 +98,7 @@ class CodesController < ApplicationController
     puts diahoy
     pvoucher.each{|d| 
       #if d.created_at.strftime("%F").to_date.month == meshoy && d.created_at.strftime("%F").to_date.day==diahoy
-          if d.panaderia==true && d.market==false && d.cod_panaderia != 0
+          if d.panaderia==true && d.market==false && d.cod_panaderia != 0 && d.cod_market == 0
             vpanaderia.push(d)
          end
       #end
